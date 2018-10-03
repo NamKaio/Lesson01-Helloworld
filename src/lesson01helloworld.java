cbe471c6ae33be618b191d8d5247f43647f41e11
NamKaio <luonghaonam99@gmail.com>
2018-10-03 10:03:24 +0700

    fist commit

---
+++ src/lesson01helloworld.java
@@ -0,0 +1,28 @@
+
+public class lesson01helloworld {
+	public static void main(String[] args) {	
+		System.out.println();
+		int a = 15;
+		int b = 10;
+		int tong = a+b;
+	System.out.println(tong);
+		int hieu = a-b;
+	System.out.println(hieu);
+		int tich = a*b;
+	System.out.println(tich);
+		int chia = a/b;
+	System.out.println(chia);
+		int chialaysodu = a%b;
+	System.out.println(chialaysodu);
+		int t1 = a=+b;
+	System.out.println(t1);
+		int t2 = a-=b;
+	System.out.println(t2);
+		int t3 = a*=b;
+	System.out.println(t3);
+		int t4 = a/=b;
+	System.out.println(t4);
+		int t5 = a%=b;
+	System.out.println(t5);
+		}
+}
